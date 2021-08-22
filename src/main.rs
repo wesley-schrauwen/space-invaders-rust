d2262278d154dc0f971211509b014291aa0cc45e
Wesley Schrauwen <wesley.schrauwen@smartly.io>
2021-08-22 12:14:05 +0300

    Working Camera + player spawn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 #![allow(unused)]
 
 use bevy::prelude::*;
-use bevy::window::WindowMode;
-use bevy::ecs::system::EntityCommands;
-use bevy::sprite::QUAD_HANDLE;
-use bevy::utils::Uuid;
-use bevy::asset::{HandleId, AssetPathId};
 
 const GAME_WINDOW: (i32, i32) = (800, 600);
 const PLAYER_SPRITE: &str = "player_1.png";
@@ -17,12 +12,7 @@ fn main() {
             width: 800.0,
             height: 600.0,
             title: "Space Invaders Rust".to_string(),
-            vsync: false,
-            resizable: false,
-            decorations: false,
-            cursor_visible: false,
-            cursor_locked: false,
-            mode: WindowMode::Windowed,
+            ..Default::default()
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(startup.system())
@@ -38,13 +28,26 @@ fn startup(
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     // reposition window to center
-    windows
-        .get_primary_mut()?
-        .set_position(IVec2::new(GAME_WINDOW.0, GAME_WINDOW.1));
+    let mut window = windows.get_primary_mut().unwrap();
+
+    let window_height = window.height() as i32;
+    let window_width = window.width() as i32;
+
+    window.set_position(
+            IVec2::new(
+                (window_height / 2) - (GAME_WINDOW.0 / 2),
+                (window_width / 2) - (GAME_WINDOW.1 / 2)
+            )
+        );
 
     // player
     commands.spawn_bundle(SpriteBundle {
         material: assets.add(asset_server.load(PLAYER_SPRITE).into()),
-        ..DefaultPlugins::default()
-    })
+        // transform: Transform {
+        //     translation: Vec3::new(),
+        //     rotation: Default::default(),
+        //     scale: Default::default()
+        // },
+        ..Default::default()
+    });
 }
