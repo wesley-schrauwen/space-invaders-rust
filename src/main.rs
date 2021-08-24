a6927ce94fe141fb3638663e9106522906b97b1c
Wesley Schrauwen <wesley.schrauwen@smartly.io>
2021-08-24 22:07:28 +0300

    Added player movement

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(unused)]
 
 use bevy::prelude::*;
+use bevy::input::keyboard::KeyboardInput;
 
 // 64x64
 const PLAYER_SPRITE: &str = "player_1.png";
+// width * height
 const WINDOW_SIZE: (f32, f32) = (800.0, 600.0);
+// one tick every second
+const ENGINE_POLL_RATE: f32 = 1.0 / 60.0;
 
 struct MaterialManifest {
     player_material: Handle<ColorMaterial>
@@ -13,6 +17,14 @@ struct MaterialManifest {
 struct GameWindowSize {
     width: f32,
     height: f32
+}
+
+struct Player;
+struct PlayerSpeed(f32);
+impl Default for PlayerSpeed {
+    fn default() -> Self {
+        Self(500.0)
+    }
 }
 
 fn main() {
@@ -30,6 +42,7 @@ fn main() {
             "actors",
             SystemStage::single(spawn_player.system())
         )
+        .add_system(player_movement.system())
         .run();
 }
 
@@ -80,5 +93,28 @@ fn spawn_player(
             ..Default::default()
         },
         ..Default::default()
-    });
+    }).insert(Player).insert(PlayerSpeed::default());
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    game_window_size: Res<GameWindowSize>,
+    mut query: Query<(&PlayerSpeed, &mut Transform, With<Player>)>
+) {
+    // handles block if single_mut is OK, no real restriction on number of players
+    if let Ok((player_speed, mut transform, player)) = query.single_mut() {
+        let change = if keyboard_input.pressed(KeyCode::Left) {
+            -1.0
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            1.0
+        } else {
+            0.0
+        };
+
+        let x_coords = transform.translation.x + change * player_speed.0 * ENGINE_POLL_RATE;
+
+        if x_coords.abs() < game_window_size.width.clone() / 2.0 - 32.0 {
+            transform.translation.x = x_coords
+        }
+    }
 }
