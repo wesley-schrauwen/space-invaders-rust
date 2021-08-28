0775f2409458271855d146bc11986e02f62f6779
Wesley Schrauwen <wesley.schrauwen@smartly.io>
2021-08-28 15:45:21 +0300

    Added collision and did some enemy struct refactoring

--- src/enemy.rs
+++ src/enemy.rs
@@ -11,15 +11,44 @@ use std::collections::btree_map::Range;
 pub struct EnemyPlugin;
 pub struct Enemy;
 
+// X and Y scales
+pub struct EnemyScale {
+    pub width: f32,
+    pub height: f32
+}
+
+pub struct EnemyDimensions {
+    pub width: f32,
+    pub height: f32
+}
+
+impl EnemyDimensions {
+    pub fn to_vec(&self) -> Vec2 {
+        return Vec2::new(self.width.clone(), self.height.clone());
+    }
+}
+
+pub const ENEMY_RAW_HEIGHT: f32 = 64.0;
+pub const ENEMY_RAW_WIDTH: f32 = 64.0;
+
+pub const ENEMY_RENDER_HEIGHT: f32 = 32.0;
+pub const ENEMY_RENDER_WIDTH: f32 = 32.0;
+
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_system_set(
             SystemSet::new().with_run_criteria(
-                FixedTimestep::step(1.0)
+                FixedTimestep::step(2.5)
             ).with_system(
                 spawn_enemy.system()
             )
-        );
+        ).insert_resource(EnemyScale {
+            width: ENEMY_RENDER_WIDTH / ENEMY_RAW_WIDTH,
+            height: ENEMY_RENDER_HEIGHT / ENEMY_RAW_HEIGHT
+        }).insert_resource(EnemyDimensions {
+            width: ENEMY_RENDER_WIDTH,
+            height: ENEMY_RENDER_HEIGHT
+        });
     }
 }
 
@@ -27,28 +56,41 @@ fn spawn_enemy(
     mut commands: Commands,
     material_manifest: Res<MaterialManifest>,
     game_window_size: Res<GameWindowSize>,
+    enemy_scale: Res<EnemyScale>,
+    enemy_dimensions: Res<EnemyDimensions>,
     mut active_enemies: ResMut<ActiveEnemies>
 ) {
+
+    if active_enemies.0 >= 5 {
+        return;
+    }
 
     active_enemies.0 += 1;
 
     let mut rng = rand::thread_rng();
 
+    let width_padding: f32 = enemy_dimensions.width.clone() / 2.0;
+    let height_padding: f32 = enemy_dimensions.height.clone() / 2.0 ;
+
     commands.spawn_bundle(SpriteBundle {
         material: material_manifest.enemy_material.clone(),
         transform: Transform {
             translation: Vec3::new(
-                rng.gen_range((-game_window_size.width.clone() / 2.0 + 64.0) .. (game_window_size.width.clone() / 2.0 - 64.0)),
-                rng.gen_range( 0.0 .. (game_window_size.height.clone() / 2.0 - 64.0)),
+                rng.gen_range((-game_window_size.width.clone() / 2.0 + &width_padding) .. (game_window_size.width.clone() / 2.0 - &width_padding)),
+                rng.gen_range( 0.0 .. (game_window_size.height.clone() / 2.0 - &height_padding)),
                 0.0
             ),
-            scale: Vec3::new(0.5, 0.5, 0.5),
+            scale: Vec3::new(enemy_scale.width.clone(), enemy_scale.height.clone(), 0.5),
             ..Default::default()
         },
         ..Default::default()
     })
         .insert(Enemy)
         .insert(Speed)
-        .insert(Laser);
+        .insert(Laser)
+        .insert(EnemyDimensions {
+            width: ENEMY_RENDER_WIDTH,
+            height: ENEMY_RENDER_HEIGHT
+        });
 
 }
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ mod enemy;
 use bevy::prelude::*;
 use bevy::input::keyboard::KeyboardInput;
 use crate::player::PlayerPlugin;
-use crate::laser::LaserPlugin;
-use crate::enemy::EnemyPlugin;
+use crate::laser::{LaserPlugin, Laser};
+use crate::enemy::{EnemyPlugin, Enemy, EnemyDimensions};
+use bevy::sprite::collide_aabb::collide;
+use bevy::math::Vec3Swizzles;
 
 // 64x64
 const PLAYER_SPRITE: &str = "player_1.png";
@@ -53,8 +55,43 @@ fn main() {
         .add_plugin(PlayerPlugin)
         .add_plugin(LaserPlugin)
         .add_plugin(EnemyPlugin)
+        .add_system(enemy_laser_collision.system())
         .add_startup_system(startup.system())
         .run();
+}
+
+fn enemy_laser_collision(
+    mut commands: Commands,
+    mut active_enemies: ResMut<ActiveEnemies>,
+    mut laser_query: Query<(Entity, &Transform, With<Laser>)>,
+    mut enemy_query: Query<(Entity, &Transform, &EnemyDimensions, With<Enemy>)>
+) {
+
+    let mut despawn_entites: Vec<Entity> = Vec::new();
+
+    for (laser_entity, laser_transform, _) in laser_query.iter_mut() {
+        for (enemy_entity, enemy_transform, enemy_dimensions, _) in enemy_query.iter_mut() {
+
+            let l_transform: &Transform = laser_transform;
+            let e_transform: &Transform = enemy_transform;
+
+            let collision = collide(l_transform.translation, l_transform.scale.xy() * 32.0, e_transform.translation, enemy_dimensions.to_vec());
+
+            if collision.is_some() {
+                despawn_entites.push(laser_entity);
+
+                if !despawn_entites.contains(&enemy_entity) {
+                    active_enemies.0 -= 1;
+                    despawn_entites.push(enemy_entity);
+                }
+            }
+
+        }
+    }
+
+    for entity in despawn_entites.iter() {
+        commands.entity(*entity).despawn();
+    }
 }
 
 fn startup(
